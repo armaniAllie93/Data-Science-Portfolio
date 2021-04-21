761c0dddebcf787beb431d880873e754c5336d47
Mani Allie <armani.allie93@gmail.com>
2021-04-21 05:01:58 +0000

    Delete coinbasefees.R

--- coinbasefees.R
+++
@@ -1,2 +0,0 @@
-cor(cryptohopper_trade_history_2020_01_16_18_16_22$`Order Amount`,cryptohopper_trade_history_2020_01_16_18_16_22$Fee, use = "complete.obs")
-plot(cryptohopper_trade_history_2020_01_16_18_16_22$`Order Amount`,cryptohopper_trade_history_2020_01_16_18_16_22$Fee,xlab = "Order Value", ylab = "Fee", main = "Coinbase Fees")
